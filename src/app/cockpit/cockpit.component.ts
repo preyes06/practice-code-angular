b523b7f43a0a3e662293e9d7426ba697bfe89fc3
Pedro Reyes <preyes@brandinstitute.com>
2021-04-08 10:12:15 -0400

    first commit

---
+++ src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,30 @@
+ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrls: ['./cockpit.component.css']
+})
+export class CockpitComponent implements OnInit {
+
+  @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  // newServerName = '';
+  newServerContent = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onAddServer(nameInput: HTMLInputElement) {
+    console.log(nameInput.value);
+   this.serverCreated.emit({serverName: nameInput.value, serverContent: this.newServerContent})
+  }
+
+  onAddBlueprint(nameInput: HTMLInputElement) {
+    console.log(nameInput.value)
+    this.blueprintCreated.emit({serverName: nameInput.value, serverContent: this.newServerContent})
+
+  }
+}
